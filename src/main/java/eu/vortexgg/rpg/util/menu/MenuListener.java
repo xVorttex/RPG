fac34de6472b06260f9c6156f682715c373dd51a
xVorttex <72511773+xVorttex@users.noreply.github.com>
2021-08-27 01:54:14 +0300

    Delete src/main directory

--- src/main/java/eu/vortexgg/rpg/util/menu/MenuListener.java
+++
@@ -1,8 +0,0 @@
-package eu.vortexgg.rpg.util.menu;
-
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
-public interface MenuListener {
-    void onClose(Player p, Inventory inv, Menu menu);
-}
