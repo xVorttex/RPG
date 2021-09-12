81dc1acade48479adc7cd73a1b4c5172982e9c0a
xVorttex <72511773+xVorttex@users.noreply.github.com>
2021-09-12 23:38:37 +0300

    Update LevelingMenu.java

--- src/main/java/eu/vortexgg/rpg/user/menu/LevelingMenu.java
+++ src/main/java/eu/vortexgg/rpg/user/menu/LevelingMenu.java
@@ -3,6 +3,8 @@ package eu.vortexgg.rpg.user.menu;
 import eu.vortexgg.rpg.user.VPlayer;
 import eu.vortexgg.rpg.util.menu.Menu;
 import org.bukkit.entity.Player;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LevelingMenu extends Menu {
